dff90fa11b0dc249035944708f8b93cd4f74ed5a
brocnroll <brocnroll18@gmail.com>
2025-12-10 14:13:51 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+// script.js
+console.log("Le JS fonctionne !");
+
+// Exemple pour le formulaire contact
+const form = document.getElementById('form');
+const confirmationMessage = document.getElementById('confirmationMessage');
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+
+  emailjs.sendForm('service_3l08cr5', 'template_f8ap0hw', this)
+    .then(function(response) {
+      confirmationMessage.innerHTML = "✅ Merci ! Votre message a bien été envoyé.";
+      confirmationMessage.style.color = "green";
+      form.reset();
+    }, function(error) {
+      confirmationMessage.innerHTML = "❌ Oups ! Une erreur est survenue : " + error.text;
+      confirmationMessage.style.color = "red";
+    });
+});
